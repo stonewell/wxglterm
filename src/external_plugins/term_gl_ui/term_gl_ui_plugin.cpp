1b07b5c555e40054f2e091d72ba95ced4db9fcde
stonewell <jingnan.si@gmail.com>
2018-01-10 20:05:18 -0800

    add scheleton of term_gl_ui and try external project of cmake

---
+++ src/external_plugins/term_gl_ui/term_gl_ui_plugin.cpp
@@ -0,0 +1,15 @@
+#include "plugin_manager.h"
+#include "plugin.h"
+
+#include "term_ui.h"
+#include "task.h"
+
+#include "freetype-gl.h"
+#include "vertex-buffer.h"
+#include "text-buffer.h"
+
+#include <GLFW/glfw3.h>
+
+extern "C"
+void register_plugins(PluginManagerPtr /*plugin_manager*/) {
+}
