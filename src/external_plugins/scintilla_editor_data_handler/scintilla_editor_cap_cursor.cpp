3b5ada32f2847b9ab431a8f73913b89f53abb155
Jingnan Si <Jingnan.Si@Gmail.com>
2018-03-07 15:58:24 -0800

    build scintilla editor as debug, add document set property cap

--- src/external_plugins/scintilla_editor_data_handler/scintilla_editor_cap_cursor.cpp
+++ src/external_plugins/scintilla_editor_data_handler/scintilla_editor_cap_cursor.cpp
@@ -3,6 +3,7 @@
 #include "cap_manager.h"
 
 DEFINE_CAP(document_home);
+DEFINE_CAP(document_set_property);
 
 void document_home(term_data_context_s & term_context,
                  const term_data_param_list & params){
@@ -11,4 +12,10 @@ void document_home(term_data_context_s & term_context,
                                                false,
                                                true);
     term_context.term_buffer->SetCol(0);
+}
+
+void document_set_property(term_data_context_s & term_context,
+                           const term_data_param_list & params){
+    term_context.term_buffer->SetProperty(params[0].str_value,
+                                          params[1].str_value);
 }
