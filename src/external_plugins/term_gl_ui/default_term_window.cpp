8eb4b24bb64ef18caf84da74b610906a6190f6c6
Jingnan Si <Jingnan.Si@Gmail.com>
2018-09-24 09:59:41 -0700

    update language default spec for font

--- src/external_plugins/term_gl_ui/default_term_window.cpp
+++ src/external_plugins/term_gl_ui/default_term_window.cpp
@@ -167,7 +167,7 @@ void DefaultTermWindow::InitFreeTypeGLContext() {
 
             auto font_size = appConfig->GetEntryUInt64("/term/font/size", 16);
             auto font_name = appConfig->GetEntry("/term/font/name", "Monospace");
-            auto font_lang = appConfig->GetEntry("/term/font/lang", "zh_cn");
+            auto font_lang = appConfig->GetEntry("/term/font/lang", "zh-CN");
 
             int height = 0;
             glfwGetFramebufferSize(m_MainDlg, NULL, &height);
