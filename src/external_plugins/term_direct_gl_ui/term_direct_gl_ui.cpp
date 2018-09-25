fa538f52d5229ece1d3e417a5623e53cf6ee6f2c
Jingnan Si <jingnan.si@gmail.com>
2018-09-24 20:16:00 -0700

    using opengl core profile

--- src/external_plugins/term_direct_gl_ui/term_direct_gl_ui.cpp
+++ src/external_plugins/term_direct_gl_ui/term_direct_gl_ui.cpp
@@ -136,6 +136,10 @@ public:
     int32_t StartMainUILoop() {
         glfwWindowHint( GLFW_VISIBLE, GL_FALSE );
         glfwWindowHint( GLFW_RESIZABLE, GL_TRUE );
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         pybind11::gil_scoped_release release;
 
