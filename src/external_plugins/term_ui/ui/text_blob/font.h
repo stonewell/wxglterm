fe078bf21b5d2556bd4ae312a65a803da2b70939
stonewell <jingnan.si@gmail.com>
2018-10-05 13:43:51 -0700

    add ensure glyph to get freetype font face for a glyph code point

--- src/external_plugins/term_ui/ui/text_blob/font.h
+++ src/external_plugins/term_ui/ui/text_blob/font.h
@@ -3,6 +3,9 @@
 #include <memory>
 #include <vector>
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 namespace fttb {
 
 class Font {
@@ -12,6 +15,7 @@ public:
 
 public:
     virtual bool IsSameFont(const std::string & desc) = 0;
+    virtual FT_Face EnsureGlyph(uint32_t codepoint) = 0;
 };
 
 using FontPtr = std::shared_ptr<Font>;
