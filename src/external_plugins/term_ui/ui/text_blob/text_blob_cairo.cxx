ba3ac37d37659fb51f17232a41d12d7fdfe5fea7
Jingnan Si <jingnan.si@gmail.com>
2018-10-07 10:16:05 -0700

    cairo color should be double and less than 1

--- src/external_plugins/term_ui/ui/text_blob/text_blob_cairo.cxx
+++ src/external_plugins/term_ui/ui/text_blob/text_blob_cairo.cxx
@@ -45,6 +45,8 @@ void wxTextBlob::DoDrawText(wxGraphicsContext * context, const FontColourCodepoi
     for(const auto & it : fcc_map) {
         auto ct = cairo_ft_font_face_create_for_ft_face (it.first, 0);
         cairo_set_font_face (native_context, ct);
+        cairo_font_extents_t fe;
+        cairo_font_extents(native_context, &fe);
 
         for(const auto & it_size : it.second) {
             cairo_set_font_size (native_context, it_size.first);
@@ -54,17 +56,17 @@ void wxTextBlob::DoDrawText(wxGraphicsContext * context, const FontColourCodepoi
                 fore.SetRGBA(it_color.first);
 
                 cairo_set_source_rgba(native_context,
-                                      fore.Red(),
-                                      fore.Green(),
-                                      fore.Blue(),
-                                      fore.Alpha());
+                                      (double)fore.Red() / 255.0,
+                                      (double)fore.Green() / 255.0,
+                                      (double)fore.Blue() / 255.0,
+                                      (double)fore.Alpha()/ 255.0);
 
                 std::vector<cairo_glyph_t> glyphs(it_color.second.size());
 
                 for(const auto & it_glyph : it_color.second) {
                     glyphs.push_back({it_glyph.index,
                                       (double)it_glyph.pt.x,
-                                      (double)it_glyph.pt.y});
+                                      (double)it_glyph.pt.y + fe.ascent});
                 }
 
                 cairo_show_glyphs(native_context, &glyphs[0], glyphs.size());
