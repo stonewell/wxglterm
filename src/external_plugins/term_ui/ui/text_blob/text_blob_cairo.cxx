63956069f65c3aa2887f7fa522725dcdc2104ab8
stonewell <jingnan.si@gmail.com>
2018-10-08 12:20:12 -0700

    cairo rendering only on linux

--- src/external_plugins/term_ui/ui/text_blob/text_blob_cairo.cxx
+++ src/external_plugins/term_ui/ui/text_blob/text_blob_cairo.cxx
@@ -1,4 +1,4 @@
-#include "text_blob.h"
+#include "text_blob_cairo.h"
 #include "wx/graphics.h"
 
 #include <iostream>
@@ -9,7 +9,37 @@
 #include <cairo.h>
 #include <cairo-ft.h>
 
-void wxTextBlob::DoDrawText(wxGraphicsContext * context, const FontColourCodepointMap & fcc_map)
+fttb::FontManagerPtr wxTextBlob::m_FontManager {fttb::CreateFontManager()};
+
+wxString wxTextBlobCairo::wxFontToFCDesc(const wxFont * pFont) {
+    wxString desc(pFont->GetFaceName());
+
+    desc << ":size=" << pFont->GetPointSize();
+
+    switch (pFont->GetStyle()) {
+    case wxFONTSTYLE_ITALIC:
+    case wxFONTSTYLE_SLANT:
+        desc << ":slant=italic";
+        break;
+    default:
+        break;
+    }
+
+    switch(pFont->GetWeight()){
+    case wxFONTWEIGHT_BOLD:
+        desc << ":weight=200";
+        break;
+    case wxFONTWEIGHT_LIGHT:
+        desc << ":weight=50";
+        break;
+    default:
+        break;
+    }
+
+    return desc;
+}
+
+void wxTextBlobCairo::DoDrawText(wxGraphicsContext * context, const FontColourCodepointMap & fcc_map)
 {
     cairo_t * native_context = (cairo_t*)context->GetNativeContext();
 
@@ -41,6 +71,56 @@ void wxTextBlob::DoDrawText(wxGraphicsContext * context, const FontColourCodepoi
                 }
 
                 cairo_show_glyphs(native_context, &glyphs[0], glyphs.size());
+            }
+        }
+    }
+}
+
+void wxTextBlobCairo::PrepareTextRendering(FontColourCodepointMap & fcc_map, BackgroundRectVector & bg_rect_vector) {
+    for(auto it = m_TextParts.begin(),
+                it_end = m_TextParts.end();
+        it != it_end;
+        it++) {
+
+        std::string desc(std::string(wxFontToFCDesc(it->pFont)));
+
+        auto font = m_FontManager->CreateFontFromDesc(desc);
+
+        if (it->back != wxNullColour) {
+            bg_rect_vector.push_back({{it->pt, it->size}, it->back});
+        }
+
+        wxArrayDouble extents;
+
+        if (m_GlyphAdvanceX == 0) {
+            m_TextExtentContext->SetFont(*it->pFont, it->fore);
+            m_TextExtentContext->GetPartialTextExtents(it->text, extents);
+        }
+
+        wxWCharBuffer buf = it->text.wc_str();
+
+        wxPoint pt = it->pt;
+
+        for(size_t i=0; i < buf.length(); i++) {
+            wchar_t ch = buf[i];
+
+            FT_Face ft_face = font->EnsureGlyph(ch);
+
+            auto p0 = fcc_map.insert(std::make_pair(ft_face,
+                                                    SizeColourCodepointMap{}));
+
+            auto p1 = p0.first->second.insert(std::make_pair(it->pFont->GetPointSize(),
+                                                             ColourCodepointMap{}));
+
+            auto p2 = p1.first->second.insert(std::make_pair((uint32_t)it->fore.GetRGBA(),
+                                                             CodepointVector{}));
+
+            p2.first->second.push_back({ch, FT_Get_Char_Index(ft_face, (FT_Long)ch), pt});
+
+            if (m_GlyphAdvanceX == 0) {
+                pt.x = it->pt.x + extents[i];
+            } else {
+                pt.x += (char_width(ch) > 1 ? 2 * m_GlyphAdvanceX : m_GlyphAdvanceX);
             }
         }
     }
