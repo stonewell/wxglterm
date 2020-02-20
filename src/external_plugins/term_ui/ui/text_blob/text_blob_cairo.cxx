e0e856943fdd0c4735f10fa0dea9a443cdf763cf
Jingnan Si <jingnan.si@gmail.com>
2020-02-19 21:41:00 -0800

    change text_blob cairo from unordered_map to forward_list

--- src/external_plugins/term_ui/ui/text_blob/text_blob_cairo.cxx
+++ src/external_plugins/term_ui/ui/text_blob/text_blob_cairo.cxx
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <forward_list>
+#include <functional>
 
 #define cairo_public
 #include <cairo.h>
@@ -50,10 +52,28 @@ typedef struct __GlyphAttrs {
     wxPoint pt;
 } GlyphAttrs;
 
-using CodepointVector = std::vector<GlyphAttrs>;
-using ColourCodepointMap = std::unordered_map<uint32_t, CodepointVector>;
-using SizeColourCodepointMap = std::unordered_map<int, ColourCodepointMap>;
-using FontColourCodepointMap = std::unordered_map<FT_Face, SizeColourCodepointMap>;
+using Codepoints = std::forward_list<GlyphAttrs>;
+
+typedef struct __ColourCodepoint {
+    uint32_t colour;
+    Codepoints code_points;
+} ColourCodepoint;
+
+using ColourCodepoints = std::forward_list<ColourCodepoint>;
+
+typedef struct __SizeColourCodepoint {
+    int size;
+    ColourCodepoints colour_codepoints;
+} SizeColourCodepoint;
+
+using SizeColourCodepoints = std::forward_list<SizeColourCodepoint>;
+
+typedef struct __FontSizeColourCodepoint {
+    FT_Face font;
+    SizeColourCodepoints size_colour_codepoints;
+} FontSizeColourCodepoint;
+
+using FontSizeColourCodepoints = std::forward_list<FontSizeColourCodepoint>;
 
 /* cairo_show_glyphs accepts runs up to 102 glyphs before it allocates a
  * temporary array.
@@ -67,7 +87,7 @@ using FontColourCodepointMap = std::unordered_map<FT_Face, SizeColourCodepointMa
 #define MAX_RUN_LENGTH 100
 
 typedef struct __RenderingData {
-    FontColourCodepointMap fcc_map;
+    FontSizeColourCodepoints fcc;
     wxTextBlob::BackgroundRectVector bg_rect_vector;
     cairo_glyph_t glyphs[MAX_RUN_LENGTH];
 } RenderingData;
@@ -86,23 +106,24 @@ void wxTextBlob::DoDrawBackground(wxGraphicsContext * context, void * rendering_
 void wxTextBlob::DoDrawText(wxGraphicsContext * context, void * rendering_data)
 {
     RenderingData * rdata = (RenderingData*)rendering_data;
-    const FontColourCodepointMap & fcc_map = rdata->fcc_map;
+    const FontSizeColourCodepoints & fcc_map = rdata->fcc;
 
     cairo_t * native_context = (cairo_t*)context->GetNativeContext();
     native_context = cairo_reference(native_context);
 
     for(const auto & it : fcc_map) {
-        auto ct = cairo_ft_font_face_create_for_ft_face (it.first, 0);
+        auto ct = cairo_ft_font_face_create_for_ft_face (it.font, 0);
         cairo_set_font_face (native_context, ct);
-        for(const auto & it_size : it.second) {
-            cairo_set_font_size (native_context, it_size.first * m_PPI.y / 72);
+
+        for(const auto & it_size : it.size_colour_codepoints) {
+            cairo_set_font_size (native_context, it_size.size * m_PPI.y / 72);
 
             cairo_font_extents_t fe;
             cairo_font_extents(native_context, &fe);
 
-            for(const auto & it_color : it_size.second) {
+            for(const auto & it_color : it_size.colour_codepoints) {
                 wxColour fore;
-                fore.SetRGBA(it_color.first);
+                fore.SetRGBA(it_color.colour);
 
                 cairo_set_source_rgba(native_context,
                                       (double)fore.Red() / 255.0,
@@ -112,7 +133,7 @@ void wxTextBlob::DoDrawText(wxGraphicsContext * context, void * rendering_data)
 
 
                 int glyph_index = 0;
-                for(const auto & it_glyph : it_color.second) {
+                for(const auto & it_glyph : it_color.code_points) {
                     rdata->glyphs[glyph_index].index = it_glyph.index;
                     rdata->glyphs[glyph_index].x = (double)it_glyph.pt.x;
                     rdata->glyphs[glyph_index].y = (double)it_glyph.pt.y + fe.ascent;
@@ -135,16 +156,42 @@ void wxTextBlob::DoDrawText(wxGraphicsContext * context, void * rendering_data)
     cairo_destroy(native_context);
 }
 
+template<typename ForwardIterator>
+ForwardIterator text_blob_find(ForwardIterator it_begin,
+                               ForwardIterator it_end,
+                               const std::function<bool(ForwardIterator)> & matcher) {
+    while(it_begin != it_end) {
+        if (matcher(it_begin))
+            return it_begin;
+
+        it_begin++;
+    }
+
+    return it_end;
+}
+
 void * wxTextBlob::BeginTextRendering() {
     RenderingData * rdata = new RenderingData;
 
-    FontColourCodepointMap & fcc_map = rdata->fcc_map;
+    FontSizeColourCodepoints & fcc_map = rdata->fcc;
     BackgroundRectVector & bg_rect_vector = rdata->bg_rect_vector;
 
     for(auto it = m_TextParts.begin(),
                 it_end = m_TextParts.end();
         it != it_end;
         it++) {
+
+        std::function<bool(SizeColourCodepoints::iterator)> find_size{
+            [it](SizeColourCodepoints::iterator it_cur) {
+                return it_cur->size == it->pFont->GetPointSize();
+            }
+        };
+
+        std::function<bool(ColourCodepoints::iterator)> find_colour{
+            [it](ColourCodepoints::iterator it_cur) {
+                return it_cur->colour == (uint32_t)it->fore.GetRGBA();
+            }
+        };
 
         std::string desc(std::string(wxFontToFCDesc(it->pFont)));
 
@@ -170,18 +217,45 @@ void * wxTextBlob::BeginTextRendering() {
 
             FT_Face ft_face = font->EnsureGlyph(ch);
 
-            auto p0 = fcc_map.insert(std::make_pair(ft_face,
-                                                    SizeColourCodepointMap{}));
+            std::function<bool(FontSizeColourCodepoints::iterator)> find_font{
+                [ft_face](FontSizeColourCodepoints::iterator it_cur) {
+                    return static_cast<void*>(ft_face) == static_cast<void*>(it_cur->font);
+                }
+            };
 
-            auto p1 = p0.first->second.insert(std::make_pair(it->pFont->GetPointSize(),
-                                                             ColourCodepointMap{}));
+            auto it_font = text_blob_find(fcc_map.begin(),
+                                          fcc_map.end(),
+                                          find_font);
 
-            auto p2 = p1.first->second.insert(std::make_pair((uint32_t)it->fore.GetRGBA(),
-                                                             CodepointVector{}));
+            if (it_font == fcc_map.end()) {
+                fcc_map.push_front({});
+                it_font = fcc_map.begin();
+                it_font->font = ft_face;
+            }
+
+            auto it_size = text_blob_find(it_font->size_colour_codepoints.begin(),
+                                          it_font->size_colour_codepoints.end(),
+                                          find_size);
+
+            if (it_size == it_font->size_colour_codepoints.end()) {
+                it_font->size_colour_codepoints.push_front({});
+                it_size = it_font->size_colour_codepoints.begin();
+                it_size->size = it->pFont->GetPointSize();
+            }
+
+            auto it_colour = text_blob_find(it_size->colour_codepoints.begin(),
+                                            it_size->colour_codepoints.end(),
+                                            find_colour);
+
+            if (it_colour == it_size->colour_codepoints.end()) {
+                it_size->colour_codepoints.push_front({});
+                it_colour = it_size->colour_codepoints.begin();
+                it_colour->colour = (uint32_t)it->fore.GetRGBA();
+            }
 
             auto char_index = FT_Get_Char_Index(ft_face, (FT_Long)ch);
 
-            p2.first->second.push_back({ch, char_index, pt});
+            it_colour->code_points.push_front({ch, char_index, pt});
 
             if (m_GlyphAdvanceX == 0) {
                 pt.x = it->pt.x + extents[i];
